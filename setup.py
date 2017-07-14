ebbf79cd856c9ef8f8a74a60033a487fc3b7760f
Petko Minkov <pminkov@gmail.com>
2017-07-14 13:32:02 -0700

    Fix license, add dependencies, bump version

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='jupyterhub-hashauthenticator',
-    version='0.1',
+    version='0.1.2',
     description='Hashed Password Authenticator for JupyterHub',
     url='https://github.com/pminkov/jupyterhub-hashauthenticator',
     author='Petko Minkov',
     author_email='pminkov@gmail.com',
     scripts=['hashauthenticator/hashauthenticator'],
+    install_requires=['jupyterhub'],
     test_suite="hashauthenticator.tests",
-    license='MIT',
+    license='BSD3',
     packages=['hashauthenticator'],
 )
